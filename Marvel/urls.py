eef7004ef2918e84e3b91649d0ee257beaac17df
Ankush Singh <ankushsahil45@gmail.com>
2022-06-30 23:14:33 +0530

    added static

--- Marvel/urls.py
+++ Marvel/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 admin.site.site_header = "Marvel Studios Admin"
 admin.site.site_title = "Marvel Studios Admin Portal"
 admin.site.index_title = "Welcome to Marvel Studios"
@@ -23,4 +26,4 @@ admin.site.index_title = "Welcome to Marvel Studios"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
