c73dbabc3561a03d06cf3ef6a1df3cb2c5273ff0
Ankush Singh <ankushsahil45@gmail.com>
2022-07-01 21:18:51 +0530

    updated urls.py

--- Marvel/urls.py
+++ Marvel/urls.py
@@ -19,19 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
-
 admin.site.site_header = "Marvel Studios Admin"
 admin.site.site_title = "Marvel Studios Admin Portal"
 admin.site.index_title = "Welcome to Marvel Studios"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("Home.urls")),
-
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    path('', include("home.urls")),
 ]
